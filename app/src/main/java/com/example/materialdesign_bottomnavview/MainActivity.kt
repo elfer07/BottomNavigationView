0b27744bee8cbbf19fde4028a3982400e5df043d
elfer07 <morenofernando@gmail.com>
2021-02-06 14:16:34 -0300

    Example Bottom Navigation View

--- app/src/main/java/com/example/materialdesign_bottomnavview/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign_bottomnavview/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.materialdesign_bottomnavview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,6 +15,29 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.container, createHomeFragment())
             .commit()
+
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        bottomNavigationView = findViewById(R.id.bottomNavView)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            val transaction = supportFragmentManager
+                    .beginTransaction()
+
+            val fragment = when (it.itemId) {
+                R.id.itemHome -> createHomeFragment()
+                R.id.itemFavorite -> createFavoriteFragment()
+                R.id.itemMap -> createMapFragment()
+                else -> createHomeFragment()
+            }
+
+            transaction
+                    .replace(R.id.container, fragment)
+                    .commit()
+            true
+        }
+
     }
 
     private fun createHomeFragment() = HomeFragment()
